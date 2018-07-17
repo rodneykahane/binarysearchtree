a03ac12e4596ffa49ddd057d13f0783fb7f3bb49
rodney <rodney@rodneykahan.es>
2018-07-17 13:35:50 +0000

    node

--- binarysearchtree/src/binarysearchtree/Node.java
+++ binarysearchtree/src/binarysearchtree/Node.java
@@ -2,4 +2,13 @@ package binarysearchtree;
 
 public class Node {
 
+	int key;
+	String value;
+	Node leftChild, rightChild;
+	
+	public Node(int key, String value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
 }
