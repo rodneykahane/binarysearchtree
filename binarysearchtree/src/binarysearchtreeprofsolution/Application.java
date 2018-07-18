f402f67aa7c600cd6541bdaf3e5c71a2d903073d
rodney <rjkahane@gmail.com>
2018-07-18 15:06:41 -0400

    adding new package for professors solution

---
+++ binarysearchtree/src/binarysearchtreeprofsolution/Application.java
@@ -0,0 +1,65 @@
+package binarysearchtreeprofsolution;	
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		BST tree = new BST();
+		/*
+				tree.insert(10,"TEN");
+				tree.insert(20,"twenty");
+				tree.insert(15,"fifteen");
+
+
+				System.out.println(tree.findMin().key);
+				System.out.println(tree.findMax().key);
+
+		 */		
+		/*tree.insert(10,"TEN");		
+				tree.insert(5,"five");
+				tree.insert(3,"three");
+				tree.insert(7,"seven");*/
+
+
+		/*tree.insert(8,"eight");		
+				tree.insert(4,"four");
+				tree.insert(2,"two");
+				tree.insert(1,"one");
+				tree.insert(6,"six");*/
+
+		/*System.out.println(tree.findMin().key);
+				System.out.println(tree.findMax().key);*/
+
+		/*tree.insert(80,"eighty");
+				tree.insert(52,"fifty two");
+				tree.insert(48,"forty eight");
+				tree.insert(71,"seventy one");
+				tree.insert(63,"sixty three");
+				tree.insert(67,"sixty seven");*/
+
+		tree.insert(50,"fifty");
+		tree.insert(25,"twenty five");
+		tree.insert(15,"fifteen");
+		tree.insert(35,"thirty five");
+		tree.insert(5,"five");
+		tree.insert(20,"twenty");
+		tree.insert(30,"thirty");
+		tree.insert(40,"forty");
+
+		/*tree.insert(50,"fifty");
+				tree.insert(65,"sixty five");
+				tree.insert(60,"sixty");
+				tree.insert(55,"fifty five");
+				tree.insert(58,"fifty eight");
+				tree.insert(70,"seventy");
+				tree.insert(67,"sixty seven");
+				tree.insert(72,"seventy two");	*/
+
+
+
+
+	}//end main
+
+}//end class
+
+
---
+++ binarysearchtree/src/binarysearchtreeprofsolution/Node.java
@@ -0,0 +1,13 @@
+package binarysearchtreeprofsolution;
+
+public class Node {
+	int key;
+	String value;
+	Node leftChild, rightChild;
+
+	public Node(int key, String value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+}
