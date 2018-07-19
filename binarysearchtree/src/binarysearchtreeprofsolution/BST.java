6bdada55e878450d6370839cd83a19916019bc05
rodney <rjkahane@gmail.com>
2018-07-19 14:41:16 -0400

    update

--- binarysearchtree/src/binarysearchtreeprofsolution/BST.java
+++ binarysearchtree/src/binarysearchtreeprofsolution/BST.java
@@ -60,6 +60,50 @@ public class BST {
 
 	//when deleting node, find the smallest node in the set of nodes that are large than the one being deleted
 	
+	public boolean remove(int key) {
+		Node currentNode = root;
+		Node parentNode = root;
+		
+		boolean isLeftChild = false;
+		
+		//searching to find the node with the key to delete
+		while(currentNode.key != key) {
+			parentNode = currentNode;
+			if(key<currentNode.key) {
+				isLeftChild=true;
+				currentNode = currentNode.leftChild;
+			} else {
+				currentNode = currentNode.rightChild;
+				isLeftChild = false;
+			}
+			if(currentNode == null) {
+				return false;
+			}
+		}
+			
+		//found the node
+		Node nodeToDelete = currentNode;
+		
+		
+		//if node is a leaf
+		if(nodeToDelete.leftChild == null && nodeToDelete.rightChild==null) {
+			if(nodeToDelete==root) {
+				root = null;
+			} else if (isLeftChild) {
+				parentNode.leftChild = null;
+			} else {
+				parentNode.rightChild = null;
+			}
+		}
+		
+		//if node has a child
+		
+		//if node has 2 children
+		
+		
+		
+		return true;
+	}//end remove()
 
 
 }//end class
